1a1f6ac11b70e98fb658df5a67cfd81304ec6cf3
Stefan Nikolov <stefan.n@sbtech.com>
2023-01-12 10:07:27 +0200

    register validation fixed

--- Js-Web-Exam/book talk/src/models/User.js
+++ Js-Web-Exam/book talk/src/models/User.js
@@ -8,18 +8,18 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
-        min: [4,'Title length is at least 4 characters!']
+        minLength: [4, 'Title length is at least 4 characters!']
     },
     email: {
         type: String,
         required: true,
         unique: true,
-        min: [10,'Title length is at least 10 characters!']
+        minLength: [10, 'Title length is at least 10 characters!']
     },
     password: {
         type: String,
         required: true,
-        min: [3,'Title length is at least 3 characters!']
+        minLength: [3, 'Title length is at least 3 characters!']
     }
 });
 
